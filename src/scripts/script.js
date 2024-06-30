f7cc3ffb5079f6132253aa3870779394a0823f1a
user223560 <rafffvvv@gmail.com>
2024-06-30 17:47:47 +0400

    hamburger button animation added

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,12 @@
-console.log("All Good")
+
+const hamburger = document.querySelector(".header__hamburger-menu")
+
+hamburger.addEventListener("click", () => {
+    console.log("Open hamburger")
+
+    if(hamburger.classList.contains("open")) {
+        hamburger.classList.remove("open")
+    } else {
+        hamburger.classList.add("open")
+    }
+})
