0807354762879d8ac06a424bc9d283d55b4a7cd5
user223560 <rafffvvv@gmail.com>
2024-07-01 18:55:42 +0400

    off canvas menu added

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,18 +1,26 @@
 
+const body = document.querySelector("body")
 const hamburger = document.querySelector(".header__hamburger-menu")
 const header = document.querySelector(".header")
 const overlay = document.querySelector(".overlay")
+const fadeElems = document.querySelectorAll(".has-fade")
 
 hamburger.addEventListener("click", () => {
     console.log("Open hamburger")
 
     if(header.classList.contains("open")) {
+        body.classList.remove("noscroll")
         header.classList.remove("open")    // Close Hamburger Menu
-        overlay.classList.remove("fade-in")
-        overlay.classList.add("fade-out")
+        fadeElems.forEach( (element) => {
+            element.classList.remove("fade-in")
+            element.classList.add("fade-out")
+        } )
     } else {
+        body.classList.add("noscroll")
         header.classList.add("open")    // Open Hamburger Menu
-        overlay.classList.remove("fade-out")
-        overlay.classList.add("fade-in")
+        fadeElems.forEach( (element) => {
+            element.classList.remove("fade-out")
+            element.classList.add("fade-in")
+        })
     }
 })
