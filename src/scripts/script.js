49d91c32b3c3a6dd78032ddf4fb9146e0db8c6ad
user223560 <rafffvvv@gmail.com>
2024-07-01 17:12:23 +0400

    hamburger animation and overlay completed

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,18 @@
 
 const hamburger = document.querySelector(".header__hamburger-menu")
 const header = document.querySelector(".header")
+const overlay = document.querySelector(".overlay")
 
 hamburger.addEventListener("click", () => {
     console.log("Open hamburger")
 
     if(header.classList.contains("open")) {
-        header.classList.remove("open")
+        header.classList.remove("open")    // Close Hamburger Menu
+        overlay.classList.remove("fade-in")
+        overlay.classList.add("fade-out")
     } else {
-        header.classList.add("open")
+        header.classList.add("open")    // Open Hamburger Menu
+        overlay.classList.remove("fade-out")
+        overlay.classList.add("fade-in")
     }
 })
